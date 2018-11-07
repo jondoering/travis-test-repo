79d15f2bc9f5b29b280019f4fc125698b0bc0f27
jonathan <jdoering@paypal.com>
2018-11-07 22:35:50 +0000

    test again

--- tests/test_travis.py
+++ tests/test_travis.py
@@ -14,7 +14,7 @@ class test_travis(unittest.TestCase):
     """Unit test for the MLDataFrame Class"""
 
     def setUp(self):
-       self.test_var = "This is a travis test for CI - failed" 
+       self.test_var = "This is a travis test for CI - failed2" 
 
     def test_init(self):
 
